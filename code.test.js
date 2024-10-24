fd1c52d9e8009d7653c54f8b2b88a96a327a6110
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-24 01:07:32 -0600

    Create code.test.js

---
+++ code.test.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const assert = require('assert');
+
+eval(fs.readFileSync('code.js') + '');
+
+var graph1 = [
+    [0,1,1],
+    [1,0,1],
+    [1,0,0]
+];
+
+var graph2 = [
+    [0,0,1],
+    [1,0,1],
+    [1,1,0]
+];
+// Isomorphic graphs, return true
+assert(JSON.stringify(are_isomorphic(graph1, graph2)) == JSON.stringify(true));
+
+var graph3 = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 0, 0]
+];
+
+var graph4 = [
+    [0, 0, 1],
+    [1, 0, 1],
+];
+// Non-isomorphic, different sizes, return false
+assert(JSON.stringify(are_isomorphic(graph3, graph4)) == JSON.stringify(false));
+
+var graph5 = [
+    [0, 0, 0],
+    [1, 0, 0],
+    [0, 0, 0]
+];
+
+var graph6 = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+];
+// Non-isomorphic, same size, different edges, return false
+assert(JSON.stringify(are_isomorphic(graph5, graph6)) == JSON.stringify(false));
+
+var graph7 = [
+    [0, 1, 0],
+    [1, 0, 0],
+    [0, 1, 0]
+];
+
+var graph8 = [
+    [0, 1, 0],
+    [1, 0, 0],
+    [0, 1, 0]
+];
+// Duplicate graphs, return true
+assert(JSON.stringify(are_isomorphic(graph7, graph8)) == JSON.stringify(true));
