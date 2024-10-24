ca64a7f599c30a19bed18c3e8b0e335072bef7b6
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-23 23:55:00 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,45 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+function are_isomorphic(graph1, graph2) {
+    graph1Size = Object.keys(graph1).length;
+    graph2Size = Object.keys(graph2).length;
+    if (graph1Size != graph2Size) {
+        return false;
+    }
+    for (i = 0; i < graph1Size; i++) {
+        node1 = graph1[i];
+        console.log("New Node 1")
+        for (j = 0; j < graph2Size; j++) {
+            result = nodeCompare(node1, graph2[j], j);
+        }
+    }
+
+    return true;
 }
+
+function nodeCompare(node1, node2, j) {
+    console.log("Node 1: ", node1);
+    console.log("Node 2: ", node2);
+    for (k = 0; k + j < node2.length; k++) {
+        stack.push(node2[k + j]);
+    }
+    for (l = 0; l < j; l++) {
+        stack.push(node2[l]);
+    }
+    console.log("Stack: ", stack);
+    return;
+}
+
+
+var graph1 = [
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 0, 0]
+];
+
+var graph2 = [
+    [0, 0, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+];
+
+are_isomorphic(graph1, graph2);
+
