98785cb85f1473827d1f4d53f6bc372316c682e5
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-24 01:06:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 function are_isomorphic(graph1, graph2) {
+    nodesMatched = 0;
     graph1Size = Object.keys(graph1).length;
     graph2Size = Object.keys(graph2).length;
     if (graph1Size != graph2Size) {
@@ -6,30 +7,26 @@ function are_isomorphic(graph1, graph2) {
     }
     for (i = 0; i < graph1Size; i++) {
         node1 = graph1[i];
-        //console.log("New Node 1")
         for (j = 0; j < graph2Size; j++) {
             node2 = graph2[j].slice();
-            console.log(node2);
             result = nodeMatch(node1, node2, i, j);
-            //console.log("Graph 1: ", graph1);
-            //console.log("Graph 2: ", graph2)
-            console.log(result);
+            if (result == true) {
+                graph2[j] = "matched";
+                nodesMatched++;
+            }
         }
     }
-    return true;
+    return (nodesMatched == graph2Size);
 }
 
 function nodeMatch(node1, node2, i, j) {
-    //console.log("Node 1: ", node1);
-    //console.log("Node 2: ", node2);
-    //console.log(node2);
-    //console.log(swappedNode);
+    if (node2 == undefined) {
+        return false;
+    }
     if (i != j) {
         node2 = swap(node2, i, j)
     }
-    //console.log("New Node 2: ", node2);
     for (k = 0; k < node1.length; k++) {
-        //console.log(node1[k], node2[k]);
         if (node1[k] != node2[k]) {
             return false;
         }
@@ -43,16 +40,3 @@ function swap(node, i, j) {
     node[j] = tmp;
     return node;
 }
-
-
-var graph1 = [
-    [0, 1, 1],
-    [1, 0, 1],
-    [1, 0, 0]
-];
-
-var graph2 = [
-    [0, 0, 1],
-    [1, 0, 1],
-    [1, 1, 0]
-];
