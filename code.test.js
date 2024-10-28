a161e5c8b0bb6512a458ad3954c32af839240621
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-28 11:31:06 -0600

    Update code.test.js
    Added tests demonstrating compatibility of differently named graphs

--- code.test.js
+++ code.test.js
@@ -62,16 +62,30 @@ var graph9 = [];
 var graph10 = [];
 assert(JSON.stringify(are_isomorphic(graph9, graph10)) == JSON.stringify(true));
 
-// New graphs demonstrating functionality with differently named nodes
+var graph10 = {
+    A: [0, 1, 0],
+    B: [1, 0, 0],
+    C: [0, 1, 0]
+};
+
 var graph11 = {
-    E: [0, 1, 1],
-    F: [1, 0, 1],
-    G: [1, 0, 0]
-};
+    E: [0, 1, 0],
+    F: [1, 0, 0],
+    G: [0, 1, 0]
+}
+// Duplicate graphs with differently named nodes
+assert(JSON.stringify(are_isomorphic(graph9, graph10)) == JSON.stringify(true));
+
 var graph12 = {
-    A: [0, 0, 1],
-    B: [1, 0, 1],
-    C: [1, 1, 0]
+    'a':            [0, 1, 1],
+    'differently':  [1, 0, 1],
+    'named graph':  [1, 0, 0]
 };
-// Isomorphic graphs, return true
-assert(JSON.stringify(are_isomorphic(graph11, graph12)) == JSON.stringify(true));
+
+var graph13 = [
+    [0, 0, 1],
+    [1, 0, 1],
+    [1, 1, 0]
+];
+// Isomorphic graphs with 1 containing named nodes and 1 containing unnamed nodes(array indexed)
+assert(JSON.stringify(are_isomorphic(graph11, graph12) == JSON.stringify(true)));
